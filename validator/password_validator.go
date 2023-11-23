3c5ada9c6bc3cad5eaf9dbc24c6f1b331ed288ef
Rian Negreiros <riannegreiros@gmail.com>
2023-11-23 15:56:52 -0300

    feat: concatenate errors
    provide more detailed information on which requirements have not been met

--- validator/password_validator.go
+++ validator/password_validator.go
@@ -4,37 +4,44 @@ package validator
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 // ValidatePassword verifica se a senha atende aos critérios de segurança
 func ValidatePassword(password string) error {
+	var errors []string
+
 	// Verificar o comprimento mínimo da senha
 	if len(password) < 8 {
-		return fmt.Errorf("A senha deve ter pelo menos 8 caracteres")
+		errors = append(errors, "A senha deve ter pelo menos 8 caracteres")
 	}
 
 	// Verificar se contém pelo menos uma letra maiúscula
 	hasUpperCase, _ := regexp.MatchString(`[A-Z]`, password)
 	if !hasUpperCase {
-		return fmt.Errorf("A senha deve conter pelo menos uma letra maiúscula")
+		errors = append(errors, "A senha deve conter pelo menos uma letra maiúscula")
 	}
 
 	// Verificar se contém pelo menos uma letra minúscula
 	hasLowerCase, _ := regexp.MatchString(`[a-z]`, password)
 	if !hasLowerCase {
-		return fmt.Errorf("A senha deve conter pelo menos uma letra minúscula")
+		errors = append(errors, "A senha deve conter pelo menos uma letra minúscula")
 	}
 
 	// Verificar se contém pelo menos um dígito numérico
 	hasDigit, _ := regexp.MatchString(`\d`, password)
 	if !hasDigit {
-		return fmt.Errorf("A senha deve conter pelo menos um dígito numérico")
+		errors = append(errors, "A senha deve conter pelo menos um dígito numérico")
 	}
 
 	// Verificar se contém pelo menos um caractere especial
 	hasSpecialChar, _ := regexp.MatchString(`[!@#$%]`, password)
 	if !hasSpecialChar {
-		return fmt.Errorf("A senha deve conter pelo menos um caractere especial (!@#$%)")
+		errors = append(errors, "A senha deve conter pelo menos um caractere especial (!@#$%)")
+	}
+
+	if len(errors) > 0 {
+		return fmt.Errorf(strings.Join(errors, "\n"))
 	}
 
 	return nil
