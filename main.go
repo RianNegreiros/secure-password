2ffc791173c31f647cb89bfe136ff5ab7b686b14
Rian Negreiros <riannegreiros@gmail.com>
2023-11-23 15:37:23 -0300

    feat: validate password

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 // PasswordRequest é a estrutura para a solicitação de senha
@@ -29,5 +32,42 @@ func validatePasswordHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	err = validatePassword(req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func validatePassword(password string) error {
+	// Verificar o comprimento mínimo da senha
+	if len(password) < 8 {
+		return fmt.Errorf("A senha deve ter pelo menos 8 caracteres")
+	}
+
+	// Verificar se contém pelo menos uma letra maiúscula
+	if strings.ToUpper(password) == password {
+		return fmt.Errorf("A senha deve conter pelo menos uma letra maiúscula")
+	}
+
+	// Verificar se contém pelo menos uma letra minúscula
+	if strings.ToLower(password) == password {
+		return fmt.Errorf("A senha deve conter pelo menos uma letra minúscula")
+	}
+
+	// Verificar se contém pelo menos um dígito numérico
+	hasDigit, _ := regexp.MatchString(`\d`, password)
+	if !hasDigit {
+		return fmt.Errorf("A senha deve conter pelo menos um dígito numérico")
+	}
+
+	// Verificar se contém pelo menos um caractere especial
+	hasSpecialChar, _ := regexp.MatchString(`[!@#$%]`, password)
+	if !hasSpecialChar {
+		return fmt.Errorf("A senha deve conter pelo menos um caractere especial (!@#$%)")
+	}
+
+	return nil
 }
